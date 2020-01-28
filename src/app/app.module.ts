93a0566251a7dd055ec332b6c767bd2f5a873bff
Suprith Aireddy <aireddysuprith@gmail.com>
2020-01-27 23:07:04 -0600

    added components for list info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router'; 
+import { RentalModule } from './rental/rental.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 
+
+
+// Adding routes 
+const routes: Routes = [
+  {path: '', redirectTo: '/rentals', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
@@ -12,7 +19,9 @@ import { HeaderComponent } from './common/header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    RentalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
